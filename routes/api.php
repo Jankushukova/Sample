a7306883601dcc8b8d703a406edd84f92e117218
air <asilkhan-al@mail.ru>
2019-08-18 19:40:16 +0600

    Materials added material_type_id

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,12 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+
+
 });
-Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
+Route::group([
+//        'middleware' => 'auth:api',
+        'prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
     Route::resource('courses', 'CoursesController', ['except' => ['create', 'edit']]);
     Route::resource('lessons', 'LessonsController', ['except' => ['create', 'edit']]);
     Route::resource('materials', 'MaterialsController', ['except' => ['create', 'edit']]);
